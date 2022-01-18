fc3ac3ab14bb1866377dff61b34018f32fb84f4a
Zhishan Li <zhishan@plus.ai>
2022-01-18 23:47:11 +0800

    correct the depthTo3d.py

--- depthTo3d.py
+++ depthTo3d.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 
 ################ FIND CHESSBOARD CORNERS - OBJECT POINTS AND IMAGE POINTS #############################
 
-chessboardSize = (9,6)
-frameSize = (640,480)
+chessboardSize = (9, 6)
+frameSize = (640, 480)
 
 
 # termination criteria
@@ -15,19 +15,22 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
-objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
+objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
 
 objp = objp * 30
-#print(objp)
+# print(objp)
 
 # Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpointsL = [] # 2d points in image plane.
-imgpointsR = [] # 2d points in image plane.
+objpoints = []  # 3d point in real world space
+imgpointsL = []  # 2d points in image plane.
+imgpointsR = []  # 2d points in image plane.
 
 
-imagesLeft = sorted(glob.glob('images/stereoLeft/*.jpg'))
-imagesRight = sorted(glob.glob('images/stereoRight/*.jpg'))
+imagesLeft = sorted(glob.glob('images/stereoLeft/*.png'))
+imagesRight = sorted(glob.glob('images/stereoRight/*.png'))
+
+print(imagesLeft)
+print(imagesRight)
 
 for imgLeft, imgRight in zip(imagesLeft, imagesRight):
 
@@ -45,10 +48,10 @@ for imgLeft, imgRight in zip(imagesLeft, imagesRight):
 
         objpoints.append(objp)
 
-        cornersL = cv.cornerSubPix(grayL, cornersL, (11,11), (-1,-1), criteria)
+        cornersL = cv.cornerSubPix(grayL, cornersL, (11, 11), (-1, -1), criteria)
         imgpointsL.append(cornersL)
 
-        cornersR = cv.cornerSubPix(grayR, cornersR, (11,11), (-1,-1), criteria)
+        cornersR = cv.cornerSubPix(grayR, cornersR, (11, 11), (-1, -1), criteria)
         imgpointsR.append(cornersR)
 
         # Draw and display the corners
@@ -56,12 +59,9 @@ for imgLeft, imgRight in zip(imagesLeft, imagesRight):
         cv.imshow('img left', imgL)
         cv.drawChessboardCorners(imgR, chessboardSize, cornersR, retR)
         cv.imshow('img right', imgR)
-        cv.waitKey(100)
+        cv.waitKey()
 
-
-cv.destroyAllWindows()
-
-
+# cv.destroyAllWindows()
 
 
 ############## CALIBRATION #######################################################
@@ -83,15 +83,13 @@ print(newCameraMatrixL)
 flags = 0
 flags |= cv.CALIB_FIX_INTRINSIC
 # Here we fix the intrinsic camara matrixes so that only Rot, Trns, Emat and Fmat are calculated.
-# Hence intrinsic parameters are the same 
+# Hence intrinsic parameters are the same
 
 criteria_stereo = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # This step is performed to transformation between the two cameras and calculate Essential and Fundamenatl matrix
-retStereo, newCameraMatrixL, distL, newCameraMatrixR, distR, rot, trans, essentialMatrix, fundamentalMatrix = cv.stereoCalibrate(objpoints, imgpointsL, imgpointsR, newCameraMatrixL, distL, newCameraMatrixR, distR, grayL.shape[::-1], criteria_stereo, flags)
-
-
-
+retStereo, newCameraMatrixL, distL, newCameraMatrixR, distR, rot, trans, essentialMatrix, fundamentalMatrix = cv.stereoCalibrate(
+    objpoints, imgpointsL, imgpointsR, newCameraMatrixL, distL, newCameraMatrixR, distR, grayL.shape[::-1], criteria_stereo, flags)
 
 
 # Reprojection Error
@@ -105,11 +103,11 @@ for i in range(len(objpoints)):
 print("total error: {}".format(mean_error/len(objpoints)))
 
 
-
 ########## Stereo Rectification #################################################
 
-rectifyScale= 1
-rectL, rectR, projMatrixL, projMatrixR, Q, roi_L, roi_R= cv.stereoRectify(newCameraMatrixL, distL, newCameraMatrixR, distR, grayL.shape[::-1], rot, trans, rectifyScale,(0,0))
+rectifyScale = 1
+rectL, rectR, projMatrixL, projMatrixR, Q, roi_L, roi_R = cv.stereoRectify(
+    newCameraMatrixL, distL, newCameraMatrixR, distR, grayL.shape[::-1], rot, trans, rectifyScale, (0, 0))
 
 stereoMapL = cv.initUndistortRectifyMap(newCameraMatrixL, distL, rectL, projMatrixL, grayL.shape[::-1], cv.CV_16SC2)
 stereoMapR = cv.initUndistortRectifyMap(newCameraMatrixR, distR, rectR, projMatrixR, grayR.shape[::-1], cv.CV_16SC2)
@@ -117,10 +115,10 @@ stereoMapR = cv.initUndistortRectifyMap(newCameraMatrixR, distR, rectR, projMatr
 print("Saving parameters!")
 cv_file = cv.FileStorage('stereoMap.xml', cv.FILE_STORAGE_WRITE)
 
-cv_file.write('stereoMapL_x',stereoMapL[0])
-cv_file.write('stereoMapL_y',stereoMapL[1])
-cv_file.write('stereoMapR_x',stereoMapR[0])
-cv_file.write('stereoMapR_y',stereoMapR[1])
+cv_file.write('stereoMapL_x', stereoMapL[0])
+cv_file.write('stereoMapL_y', stereoMapL[1])
+cv_file.write('stereoMapR_x', stereoMapR[0])
+cv_file.write('stereoMapR_y', stereoMapR[1])
 
 cv_file.release()
 
@@ -134,20 +132,19 @@ stereoMapL_y = cv_file.getNode('stereoMapL_y').mat()
 stereoMapR_x = cv_file.getNode('stereoMapR_x').mat()
 stereoMapR_y = cv_file.getNode('stereoMapR_y').mat()
 
-imgL = cv.imread('images/stereoLeft/left10.jpg', cv.IMREAD_GRAYSCALE)
-imgR = cv.imread('images/stereoRight/right10.jpg', cv.IMREAD_GRAYSCALE)
+imgL = cv.imread('images/stereoLeft/imageL6.png', cv.IMREAD_GRAYSCALE)
+imgR = cv.imread('images/stereoRight/imageR6.png', cv.IMREAD_GRAYSCALE)
 
 # Show the frames
-cv.imshow("frame right", imgR) 
+cv.imshow("frame right", imgR)
 cv.imshow("frame left", imgL)
 
-
- # Undistort and rectify images
+# Undistort and rectify images
 imgR = cv.remap(imgR, stereoMapR_x, stereoMapR_y, cv.INTER_LANCZOS4, cv.BORDER_CONSTANT, 0)
 imgL = cv.remap(imgL, stereoMapL_x, stereoMapL_y, cv.INTER_LANCZOS4, cv.BORDER_CONSTANT, 0)
-                    
+
 # Show the frames
-cv.imshow("frame right", imgR) 
+cv.imshow("frame right", imgR)
 cv.imshow("frame left", imgL)
 
 
@@ -156,10 +153,9 @@ stereo = cv.StereoBM_create(numDisparities=32, blockSize=9)
 # Larger block size implies smoother, though less accurate disparity map
 disparity = stereo.compute(imgL, imgR)
 
-#print(depPth)
+print(disparity)
 
 image_3d_reprojection = cv.reprojectImageTo3D(disparity, Q, handleMissingValues=True)
-
 
 
 cv.imshow("Left", imgL)
@@ -168,8 +164,7 @@ cv.imshow("Disparity", disparity)
 cv.imshow("Reprojection", image_3d_reprojection)
 
 
-cv.waitKey(0)
+cv.waitKey()
 plt.imshow(disparity)
 plt.axis('off')
 plt.show()
-
